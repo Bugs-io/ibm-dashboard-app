16e000fb698c1be7abb8424815a9c7947cb743ee
CristianCazares <cm_cjavier@hotmail.com>
2023-04-21 11:11:39 -0600

    feat(login): added head

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LoginStep1, LoginStep2, LandingLayout } from "@/components";
+import Head from "next/head";
 
 const Login = () => {
   const [loginStep, setLoginStep] = useState(1);
@@ -12,16 +13,21 @@ const Login = () => {
   };
 
   return (
-    <LandingLayout>
-      {loginStep === 1 ? (
-        <LoginStep1 switchLoginStep={swtichLoginStep} />
-      ) : (
-        <LoginStep2
-          handleClick={handleLogin}
-          swtichLoginStep={swtichLoginStep}
-        ></LoginStep2>
-      )}
-    </LandingLayout>
+    <>
+    <Head>
+      <title>IBM Strategic Dashboard</title>
+    </Head>
+      <LandingLayout>
+        {loginStep === 1 ? (
+          <LoginStep1 switchLoginStep={swtichLoginStep} />
+        ) : (
+          <LoginStep2
+            handleClick={handleLogin}
+            swtichLoginStep={swtichLoginStep}
+          ></LoginStep2>
+        )}
+      </LandingLayout>
+    </>
   );
 };
 
