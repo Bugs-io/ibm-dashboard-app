b296cd16b42dd098a8287c9dfe14f570bc953b6e
CristianCazares <cm_cjavier@hotmail.com>
2023-04-23 18:04:14 -0600

    feat(auth): Improved two step login data conservation

--- src/components/LoginSteps/loginStep1.tsx
+++ src/components/LoginSteps/loginStep1.tsx
@@ -1,4 +1,4 @@
-import { TextInput, Button, Checkbox } from "carbon-components-react";
+import { TextInput, Button, Checkbox, Form } from "carbon-components-react";
 import { ArrowRight } from "@carbon/icons-react";
 import styles from "@/styles/LandingForm.module.scss";
 
@@ -6,8 +6,8 @@ import CarbonLink from "../CarbonLink";
 import { UserData } from "@/utils/SignUpValidations/types";
 
 interface Props {
-  handleClick: () => void;
-  handleChange: (field: string, e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   inputError: string;
   loginData: UserData;
 }
@@ -24,40 +24,44 @@ const LoginStep1 = (props: Props) => {
       </h4>
 
       <div className={styles.divider} />
+      <Form>
+        <div className={styles.form}>
+          <div className={styles.inputLabelContainer}>
+            <p>Continue with IBMid</p>
+            <p className={styles.inputLabelLink}>
+              <CarbonLink
+                href="https://www.ibm.com/ibmid/myibm/help/us/helpdesk.html"
+                target="_blank"
+              >
+                Forgot ID?
+              </CarbonLink>
+            </p>
+          </div>
 
-      <div className={styles.form}>
-        <div className={styles.inputLabelContainer}>
-          <p>Continue with IBMid</p>
-          <p className={styles.inputLabelLink}>
-            <CarbonLink
-              href="https://www.ibm.com/ibmid/myibm/help/us/helpdesk.html"
-              target="_blank"
-            >
-              Forgot ID?
-            </CarbonLink>
-          </p>
+          <TextInput
+            id="login-email"
+            name="email"
+            value={props.loginData.email}
+            labelText=""
+            placeholder="username@ibm.com"
+            onChange={(e) => props.handleChange(e)}
+            invalid={props.inputError !== ""}
+            invalidText={props.inputError}
+          />
         </div>
-        <TextInput
-          id="login-username"
-          value={props.loginData.email}
-          labelText=""
-          placeholder="username@ibm.com"
-          onChange={(e) => props.handleChange("email", e)}
-          invalid={props.inputError !== ""}
-          invalidText={props.inputError}
-        />
-      </div>
-      <div style={{ marginBottom: 16 }}>
-        <Button
-          kind="primary"
-          renderIcon={ArrowRight}
-          onClick={() => props.handleClick()}
-          className={styles.buttonContainer}
-        >
-          Continue
-        </Button>
-      </div>
-      <Checkbox id="check-remember-id" labelText="Remember ID?" />
+        <div style={{ marginBottom: 16 }}>
+          <Button
+            kind="primary"
+            renderIcon={ArrowRight}
+            onClick={(e) => props.handleSubmit(e)}
+            className={styles.buttonContainer}
+            type="submit"
+          >
+            Continue
+          </Button>
+        </div>
+        <Checkbox id="check-remember-id" labelText="Remember ID?" />
+      </Form>
     </div>
   );
 };
