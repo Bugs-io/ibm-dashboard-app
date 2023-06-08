e955102d6ea4dc82adfb2a77cd1433bec1e1ab71
CristianCazares <cm_cjavier@hotmail.com>
2023-06-07 18:12:07 -0600

    feat(topIndustryCourses): changed to a table instead of barChart

--- src/charts/TopIndustryCourses.tsx
+++ src/charts/TopIndustryCourses.tsx
@@ -1,42 +1,65 @@
 import { useState, useEffect } from "react";
-import { SimpleBarChart } from "@carbon/charts-react";
-import { BarChartOptions, ChartTabularData } from "@carbon/charts/interfaces";
+import {
+  DataTable,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
+  DataTableSkeleton,
+} from "carbon-components-react";
 import GraphCard from "@/components/GraphCard";
 import { ChartProps } from "@/utils/chartOptions";
 import useClient from "@/hooks/useClient";
 
-const graphOptions: BarChartOptions = {
-  axes: {
-    left: {
-      mapsTo: "group",
-      // @ts-expect-error
-      scaleType: "labels",
-    },
-    bottom: {
-      mapsTo: "value",
-    },
-  },
-  height: "400px",
-  // @ts-expect-error
-  theme: "g90",
-  data: {
-    loading: true,
-  },
+type Course = { group: string; value: string };
+
+type TableRowType = {
+  id: string;
+  name: string;
+  status: string;
 };
 
-const LOADING_STATE_TEXT = "Consulting third party sources. . .";
+const NUMBER_OF_COURSES_ON_TOP = 5;
+
+const tableHeaders = [
+  {
+    key: "id",
+    header: "Top",
+  },
+  {
+    key: "name",
+    header: "Course",
+  },
+  {
+    key: "status",
+    header: "Attendees",
+  },
+];
+
+const parseCourses = (responseCourses: Course[]) => {
+  const topNCourses = responseCourses.slice(0, NUMBER_OF_COURSES_ON_TOP);
+  const parsed: TableRowType[] = topNCourses.map((course, i) => ({
+    id: (i + 1).toString(),
+    name: course.group,
+    status: course.value,
+  }));
+
+  return parsed;
+};
 
 const TopIndustryCourses = ({ id, isInteractive }: ChartProps) => {
   const client = useClient();
-  const [data, setData] = useState<ChartTabularData>([]);
+  const [data, setData] = useState<TableRowType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [options, setOptions] = useState<BarChartOptions>(graphOptions);
 
   const getTopIndustryCourses = async () => {
     try {
       setIsLoading(true);
       const response = await client.getTopIndustryCourses();
-      setData(response);
+      console.log(parseCourses(response));
+      setData(parseCourses(response));
     } catch (error) {
       console.log(error);
     } finally {
@@ -48,21 +71,62 @@ const TopIndustryCourses = ({ id, isInteractive }: ChartProps) => {
     getTopIndustryCourses();
   }, []);
 
-  useEffect(() => {
-    setOptions((prevOptions) => ({
-      ...prevOptions,
-      data: { loading: isLoading },
-      title: isLoading ? LOADING_STATE_TEXT : "",
-    }));
-  }, [isLoading]);
-
   return (
     <GraphCard
       title="Top courses from the industry"
       id={id}
       isInteractive={isInteractive}
     >
-      <SimpleBarChart data={data} options={options} />
+      <div style={{ marginBottom: 16 }} />
+
+      {isLoading ? (
+        <DataTableSkeleton
+          headers={tableHeaders}
+          rowCount={NUMBER_OF_COURSES_ON_TOP}
+          columnCount={tableHeaders.length}
+          compact
+          showToolbar={false}
+          showHeader={false}
+        />
+      ) : (
+        // @ts-expect-error
+        <DataTable rows={data} headers={tableHeaders} isSortable>
+          {
+            // @ts-expect-error
+            ({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
+              <Table {...getTableProps()}>
+                <TableHead>
+                  <TableRow>
+                    {
+                      // @ts-expect-error
+                      headers.map((header) => (
+                        <TableHeader {...getHeaderProps({ header })}>
+                          {header.header}
+                        </TableHeader>
+                      ))
+                    }
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {
+                    // @ts-expect-error
+                    rows.map((row) => (
+                      <TableRow {...getRowProps({ row })}>
+                        {
+                          // @ts-expect-error
+                          row.cells.map((cell) => (
+                            <TableCell key={cell.id}>{cell.value}</TableCell>
+                          ))
+                        }
+                      </TableRow>
+                    ))
+                  }
+                </TableBody>
+              </Table>
+            )
+          }
+        </DataTable>
+      )}
     </GraphCard>
   );
 };
